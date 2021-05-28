7597a96ffe6d40d78768a16d49154ee86ee0b83e
Martijn <viziousdev@gmail.com>
2021-05-28 14:31:15 +0200

    some changes.

--- src/app/pool/create/create.component.ts
+++ src/app/pool/create/create.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  submitForm() {
+    // redirect user to the pool
+    this.router.navigate(['pool/1/dashboard']);
   }
-
 }
